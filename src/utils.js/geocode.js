e9bd0d7eb828e19ef2241575017a45d7f7df9f0a
unknown <sayantikauem@gmail.com>
2023-06-07 12:10:01 +0530

    fixed an error in geocode

--- src/utils.js/geocode.js
+++ src/utils.js/geocode.js
@@ -3,7 +3,7 @@ const request = require('postman-request')
 const geocode = (address, callback) =>{
 
     const url = 'https://api.geoapify.com/v1/geocode/search?text='+ encodeURIComponent(address) + '&apiKey=5c636098705547e5a310992fd35012b5'
-    request({url:url,json:true},(error,{body})=>{
+    request({url:url,json:true},(error,{body}={})=>{
         if(error)
         {
             callback("Unable to connect to location services",undefined)
